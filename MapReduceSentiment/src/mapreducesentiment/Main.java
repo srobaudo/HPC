8b96ff4e37a5fab78e40846eb136f45c53c603c3
Sergio Robaudo <srobaudo@gmail.com>
2014-12-27 13:49:32 -0200

    main

--- MapReduceSentiment/src/mapreducesentiment/Main.java
+++ MapReduceSentiment/src/mapreducesentiment/Main.java
@@ -3,14 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package mapreducesentiment;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.BytesWritable;
-import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
@@ -19,39 +17,29 @@ import org.apache.hadoop.util.ToolRunner;
 
 /**
  *
- * @author Lu
+ * @author cami
  */
-public class Main extends Configured implements Tool{
+public class Main extends Configured implements Tool {
 
     public static void main(String[] args) throws Exception {
         int res = ToolRunner.run(new Main(), args);
-        System.exit(res);       
+        System.exit(res);
     }
 
     @Override
     public int run(String[] args) throws Exception {
-//        if (args.length != 2) {
-//            System.out.println("usage: [input] [output]");
-//            System.exit(-1);
-//        }
+        Job job = new Job(new Configuration(), "sentiment");
 
-        Job job = new Job(new Configuration(), "mcell");
-        job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(BytesWritable.class);
+        job.setOutputKeyClass(SentimentKeyWritableComparable.class);
+        job.setOutputValueClass(LongWritable.class);
 
         job.setMapperClass(SentimentMapper.class);
-        //job.setReducerClass(VoteCountReducer.class);
-        job.setNumReduceTasks(0);
-        
-        //job.setInputFormatClass(WholeFileInputFormat.class);
-        //job.setOutputFormatClass(TextOutputFormat.class);
+        job.setReducerClass(SentimentReducer.class);
 
-        //System.out.println(args[0]);
-        
+        job.setInputFormatClass(MovieCommentInputFormat.class);
+
         FileInputFormat.setInputPaths(job, new Path("wasb:///Scene.main.mdl"));//args[0]));
-        FileOutputFormat.setOutputPath(job, new Path("wasb:///mcell/test/output"));//args[1]));
-        
-        job.addCacheFile(new Path("wasb:///mcell.exe").toUri());
+        FileOutputFormat.setOutputPath(job, new Path("wasb:///sentiment/test/output"));//args[1]));
 
         job.setJarByClass(Main.class);
 
